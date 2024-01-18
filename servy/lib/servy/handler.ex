2f9acd59191645f2771eae1d4fd8abbb5d097ea5
JanoBourian <supermath33@hotmail.com>
2024-01-18 07:19:05 +0000

    Add: Using transformation and rethinking the OOP to transform our transactions in functional operations

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,36 +1,3 @@
-# 2.- Create Mix Project
-
-Basic steps to create, edit and run an elixir project.
-
-```bash
-mix new servy
-mix help
-cd servy
-mix test
-```
-
-Information: 
-
-* mix.exs is the configuration file for our dependencies and requirements
-* .exs: such as scripts
-* .ex: compiled files
-* to execute a script _elixir/lib/servy.ex_ 
-
-If you want to have available your scripts in the interactive shell you can code:
-
-```bash
-iex -S mix
-c "lib/servy.ex"
-iex lib/servy.ex
-String.reverse("keep")
-Servy.hello("Francisco")
-Testing.sum_operation(37, 73)
-r Servy
-```
-
-3.- High Level Transformation
-
-```elixir
 defmodule Servy.Handler do
     def handle(request) do
         # conv = parse(request)
@@ -69,16 +36,3 @@ request = ""
 response = Servy.Handler.handle(request)
 
 IO.puts response
-```
-
-```
-```
-
-```
-```
-
-```
-```
-
-```
-```
