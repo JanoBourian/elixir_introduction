ded6d71d65c150f8ed55d93d8df35373dd68cae0
JanoBourian <supermath33@hotmail.com>
2024-02-13 06:30:58 +0000

    add: topics review

--- servy/lib/response/response.ex
+++ servy/lib/response/response.ex
@@ -7,48 +7,68 @@ defmodule Servy.Response do
         |> rewrite_path
         # log
         |> log
-        # get status code
-        |> get_status
-        # get response body
-        |> get_body
+        # Body based on method
+        |> get_method
         # final response
         |> format
     end
     
     def parse(request) do
-        conv = %{ method: "GET", 
-        path: "/path", 
+        [method, path, _] =
+            request
+            |> String.split("\n")
+            |> List.first
+            |> String.split(" ")
+        %{ method: method, 
+        path: path, 
         response_body: "", 
         status_code: nil}
     end
     
+    def rewrite_path(%{ path: "/dardo"} = conv) do
+        conv
+    end
+    
     def rewrite_path(conv) do
-        conv = %{ method: "GET", 
-        path: "/valid_path", 
-        response_body: "", 
-        status_code: nil}
+        %{ conv | path: "/forbidden"}
     end
     
     def log(conv) do
         IO.inspect conv
     end
     
-    def get_status(conv) do
-        conv = %{conv | status_code: 200}
+    def get_method(%{ method: "GET" } = conv) do
+        %{conv | response_body: "GET Method", status_code: 200}
     end
     
-    def get_body(conv) do
-        conv = %{conv | response_body: "Hello"}
+    def get_method(%{ method: "POST" } = conv) do
+        %{conv | response_body: "POST Method", status_code: 201}
     end
+    
+    def get_method(conv) do
+        %{conv | response_body: "Forbidden", status_code: 403}
+    end
+    
     
     def format(conv) do
         """
-        HTTP/1.1 200 OK
+        HTTP/1.1 #{conv.status_code} #{status_reason(conv.status_code)}
         Content-Type: text/html
-        Content-Length: 25
+        Content-Length: #{String.length(conv.response_body)}
 
-        Here is the response body
+        #{conv.response_body}
         """
+    end
+    
+    defp status_reason(code) do
+        %{
+            200 => "OK",
+            201 => "Created",
+            401 => "Unauthorized",
+            403 => "Forbidden",
+            404 => "Not Found",
+            500 => "Internal Server Error"
+        }[code]
     end
 end
 
@@ -65,6 +85,17 @@ IO.puts response
 
 request = """
 GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Response.handle(request)
+IO.puts response
+
+request = """
+PUT /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
