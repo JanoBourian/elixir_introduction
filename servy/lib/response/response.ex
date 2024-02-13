483727f9bc374665f962cbc3975d50017493b642
JanoBourian <supermath33@hotmail.com>
2024-02-13 02:24:48 +0000

    add: first functional approach

--- servy/lib/response/response.ex
+++ servy/lib/response/response.ex
@@ -2,11 +2,53 @@ defmodule Servy.Response do
     def handle(request) do
         request
         # get method and path
+        |> parse
         # renamed non existing path
+        |> rewrite_path
         # log
+        |> log
         # get status code
+        |> get_status
         # get response body
+        |> get_body
         # final response
+        |> format
+    end
+    
+    def parse(request) do
+        conv = %{ method: "GET", 
+        path: "/path", 
+        response_body: "", 
+        status_code: nil}
+    end
+    
+    def rewrite_path(conv) do
+        conv = %{ method: "GET", 
+        path: "/valid_path", 
+        response_body: "", 
+        status_code: nil}
+    end
+    
+    def log(conv) do
+        IO.inspect conv
+    end
+    
+    def get_status(conv) do
+        conv = %{conv | status_code: 200}
+    end
+    
+    def get_body(conv) do
+        conv = %{conv | response_body: "Hello"}
+    end
+    
+    def format(conv) do
+        """
+        HTTP/1.1 200 OK
+        Content-Type: text/html
+        Content-Length: 25
+
+        Here is the response body
+        """
     end
 end
 
