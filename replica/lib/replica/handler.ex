df8d9c24987312c9099c2a38b73fd06a07a3c1a2
JanoBourian <supermath33@hotmail.com>
2024-02-21 05:03:58 +0000

    add: module attributes

--- replica/lib/replica/handler.ex
+++ replica/lib/replica/handler.ex
@@ -1,4 +1,9 @@
 defmodule Replica.Handler do
+    @moduledoc "Handles HTTP requests."
+    
+    @pages_path Path.expand("../../pages", __DIR__)
+    
+    @doc "Transform the request into a response."
     def handle(request) do
         request
         |> parse()
@@ -33,7 +38,7 @@ defmodule Replica.Handler do
     def log(conv), do: IO.inspect conv
     
     def route(%{ method: "GET", path: "/about"} = conv) do
-        Path.expand("../../pages", __DIR__)
+        @pages_path
         |> Path.join("about.html")
         |> File.read()
         |> handle_file(conv)
