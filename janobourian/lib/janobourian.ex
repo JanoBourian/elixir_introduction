428a89c890be374e697b4e341d4dd675dacfc84d
JanoBourian <supermath33@icloud.com>
2024-02-22 21:45:33 -0600

    add: new mix project named janobourian

---
+++ janobourian/lib/janobourian.ex
@@ -0,0 +1,20 @@
+defmodule Janobourian do
+  @moduledoc """
+  Documentation for `Janobourian`.
+  """
+
+  @message "Hello " <> "janobourian"
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Janobourian.hello()
+      :world
+
+  """
+  def hello do
+    @message
+  end
+end
---
+++ janobourian/test/janobourian_test.exs
@@ -0,0 +1,8 @@
+defmodule JanobourianTest do
+  use ExUnit.Case
+  doctest Janobourian
+
+  test "greets the world" do
+    assert Janobourian.hello() == :world
+  end
+end
