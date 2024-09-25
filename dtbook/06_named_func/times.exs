4a2d0d488413ae02a9c40645ade3ac3ae7db2d8d
JanoBourian <supermath33@icloud.com>
2024-09-24 23:21:54 -0600

    add: Factorial function

--- dtbook/06_named_func/times.exs
+++ dtbook/06_named_func/times.exs
@@ -3,4 +3,10 @@ defmodule Times do
   def double(_), do: IO.puts("Not an integer")
 end
 
+defmodule Factorial do
+  def factorial(0), do: 1
+  def factorial(n) when is_integer(n) and n > 0, do: n*factorial(n-1)
+end
+
 Times.double(5)
+IO.puts Factorial.factorial(1990)
