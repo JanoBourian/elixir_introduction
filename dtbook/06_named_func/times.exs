922ba43dbbb3bfa60f739e1ed0d6957b7ca137ed
JanoBourian <supermath33@hotmail.com>
2024-09-27 00:30:56 +0000

    add: information about default parameters

--- dtbook/06_named_func/times.exs
+++ dtbook/06_named_func/times.exs
@@ -11,3 +11,13 @@ end
 
 Times.double(5)
 IO.puts Factorial.factorial(1990)
+
+defmodule Example do
+  def func(p1, p2 \\ 2, p3 \\ 3, p4) do
+    IO.puts(inspect([p1, p2, p3, p4]))
+  end
+end
+
+Example.func("a", "b")
+Example.func("a", "b", "c")
+Example.func("a", "b", "c", "d")
