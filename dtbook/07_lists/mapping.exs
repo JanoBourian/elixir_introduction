9a7f1fa45149d91cbf1b6c7d94cd43f6e238eec1
JanoBourian <supermath33@icloud.com>
2024-10-10 00:44:51 -0600

    add: some pattern matching and recursion

--- dtbook/07_lists/mapping.exs
+++ dtbook/07_lists/mapping.exs
@@ -3,6 +3,23 @@ defmodule Mapping do
   def reduce([head|tail], value, function) do
     reduce(tail, function.(value,head), function )
   end
+
+  def mapsum([], _function), do: 0
+  def mapsum([head|tail], function) do
+    function.(head) + mapsum(tail, function)
+  end
+
+  def max([single_element]), do: single_element
+  def max([head|tail]) do
+    max_tail = max(tail)
+    if head > max_tail do
+      head
+    else
+      max_tail
+    end
+  end
 end
 
 IO.puts(Mapping.reduce([1,2,3], 0, fn (a,b) -> a + b end))
+IO.puts(Mapping.mapsum([1,2,3], &(&1*&1)))
+IO.puts(Mapping.max([17,19,3,2,5,7,13]))
