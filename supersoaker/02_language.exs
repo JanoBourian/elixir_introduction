dbc428811b527c00fd1490b8f85c84697858cf7c
JanoBourian <supermath33@hotmail.com>
2025-01-21 11:08:48 -0600

    add: integer module and operations

--- supersoaker/02_language.exs
+++ supersoaker/02_language.exs
@@ -6,8 +6,16 @@ defmodule Supersoaker.Language do
   def atoms do
     :ok
   end
+
+  def integer_convertion(number) do
+    IO.puts Integer.to_string(number)
+    IO.puts Integer.to_charlist(number)
+    IO.puts inspect Integer.digits(number)
+    IO.puts Integer.gcd(number, 3)
+  end
 end
 
 Supersoaker.Language.hello
 IO.puts Supersoaker.Language.area(2, 3)
 IO.puts Supersoaker.Language.atoms
+Supersoaker.Language.integer_convertion(17)
